f31c17178c5436a8b3a2e03afd81c935580b706f
R. Kukuh <rkukuh@gmail.com>
2022-03-10 06:43:32 +0700

    Initial Commit

---
+++ swiftui3-features/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui3-features
+//
+//  Created by R. Kukuh on 10/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui3-features/swiftui3_featuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui3_featuresApp.swift
+//  swiftui3-features
+//
+//  Created by R. Kukuh on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui3_featuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
