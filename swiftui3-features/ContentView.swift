121162bda54e790ee05b75ea7abcb2aa846ced58
R. Kukuh <rkukuh@gmail.com>
2022-03-11 06:20:41 +0700

    Use async image component

--- swiftui3-features/ContentView.swift
+++ swiftui3-features/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let url = URL(string: "https://pict.sindonews.net/dyn/620/pena/news/2020/04/24/716/7279/sony-pictures-bakal-garap-film-live-action-dari-anime-one-punch-man-gel.jpg")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            AsyncImage(url: url)
+                .scaledToFit()
+                .navigationTitle("Async Image")
+            
+        }
+        
     }
 }
 
